bd142951d04c0777d9845bc11462f9b91a6c8844
TheGB0077 <39922116+TheGB0077@users.noreply.github.com>
2023-10-22 00:17:55 -0300

    Update index.ts [skip-ci]

--- tech-educa/.vitepress/theme/index.ts
+++ tech-educa/.vitepress/theme/index.ts
@@ -1,23 +1,27 @@
+// Vue / Vitepress
+import DefaultTheme from "vitepress/theme";
+import type {Theme} from "vitepress";
+import "./styles/index.scss";
+import { h } from "vue";
+
+//Componentes
 import ManterPWA from "@theme/components/PWA/ManterPWA.vue";
 import NotFound from "@theme/components/404.vue";
 import Link from "@theme/components/Link.vue";
 import Youtube from "@theme/components/Youtube.vue";
 import FonteImg from "@theme/components/FonteImg.vue";
-import DefaultTheme from "vitepress/theme";
-import "./styles/index.scss";
-import { h } from "vue";
 
 export default {
-  ...DefaultTheme,
+  extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
       "not-found": () => h(NotFound),
       "layout-bottom": () => h(ManterPWA),
     });
   },
-  enhanceApp({ app }: { app: any }) {
+  enhanceApp({ app }) {
     app.component("Link", Link);
     app.component("Youtube", Youtube);
     app.component("FonteImg", FonteImg);
   },
-};
+} satisfies Theme;
